88afefa78befa59ee8a7566143e7836031ee6cd5
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-10-22 11:32:05 -0300

    Adicionado filmes Ação

--- models/ModelAcao.js
+++ models/ModelAcao.js
@@ -1,16 +1,16 @@
 //Conexão com array de Filmes!
-const comedia = require("../src/data/comedia.json");
+const acao = require("../src/data/acao.json");
 
 module.exports = {
   //Pegar Todos os filmes do Array!
   getAll() {
-    const film = comedia;
+    const film = acao;
     return film;
   },
 
   //Pegar apenas um filme do Array!
   getFindOne(id) {
-    const filme = comedia.find((film) => film.id === parseInt(id));
+    const filme = acao.find((film) => film.id === parseInt(id));
     return filme;
   },
 };
--- routes/filmesRoutes.js
+++ routes/filmesRoutes.js
@@ -5,6 +5,7 @@ const ControllerBanner = require("../controllers/ControllerBanner");
 //Filmes
 const ControllerFilmes = require("../controllers/ControllerFilmes");
 const ControllerFilmesComedia = require("../controllers/ControllerComedia");
+const ControllerAcao = require("../controllers/ControllerAcao");
 
 //Endpoint do Banner
 router.get("/banner", ControllerBanner.getAll);
@@ -14,4 +15,7 @@ router.get("/filmes/:id", ControllerFilmes.getFindOne);
 //Endpoint dos Filmes de Comédia
 router.get("/comedia", ControllerFilmesComedia.getAll);
 router.get("/comedia/:id", ControllerFilmesComedia.getFindOne);
+//Endpoint dos Filmes de Ação
+router.get("/acao", ControllerAcao.getAll);
+router.get("/acao/:id", ControllerAcao.getFindOne);
 module.exports = router;
